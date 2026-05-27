79491d741ac8418fdddc85e1e5077a2516203048
SRay2005 <sannidhya.ray2005@gmail.com>
2026-05-28 03:03:22 +0530

    fix: added missing imports

--- components/TransitionProvider.tsx
+++ components/TransitionProvider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { useRouter } from "next/navigation"
-import { createContext, useContext, useState } from "react"
+import { useRouter, usePathname } from "next/navigation"
+import { createContext, useContext, useState, useEffect } from "react"
 import PortalTransition from "./PortalTransition"
 
 const TransitionContext = createContext<any>(null)
